c8b3d2b736384dd6393d5eec178aa455d119f986
wangzk <biocory42@gmail.com>
2025-08-18 18:54:11 +0800

    准备进行多节点复测

--- run2node.sh
+++ run2node.sh
@@ -2,9 +2,6 @@
 
 # docker run -itd --runtime=nvidia --gpus all --device=/dev/infiniband --shm-size 1024G --ulimit memlock=-1  --network overlay01 --ip 192.168.0.100 -v /data:/data -v /root:/root -v /root/.ssh/:/root/.ssh/ 99a
 # ssh gpu72 "docker run -itd --runtime=nvidia --gpus all --device=/dev/infiniband --shm-size 1024G --ulimit memlock=-1  --network overlay01 --ip 192.168.0.101 -v /data:/data -v /root:/root -v /root/.ssh/:/root/.ssh/ 99a"
-
-# docker run -itd --runtime=nvidia --gpus all --device=/dev/infiniband --shm-size 1024G --ulimit memlock=-1  --network overlay01 --ip 192.168.0.200 -v /data:/data -v /root:/root -v /root/.ssh/:/root/.ssh/ 30db
-
 # docker exec -it `docker ps -q` bash /data/llamafactory/run.sh 0 2>&1|tee run2node_gpu64.log
 # docker exec -it `docker ps -q` bash /data/llamafactory/run.sh 1 2>&1|tee run2node_gpu72.log
 
@@ -19,5 +16,4 @@ export NCCL_IB_DISABLE="0"
 export NCCL_SOCKET_IFNAME=eth0
 export OMP_NUM_THREADS=16
 
-# FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=$1 MASTER_ADDR=192.168.0.100 MASTER_PORT=29500 llamafactory-cli train run2node.yaml
-FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=$1 MASTER_ADDR=192.168.0.100 MASTER_PORT=29500 llamafactory-cli train llama3_full_sft.yaml
+FORCE_TORCHRUN=1 NNODES=2 NODE_RANK=$1 MASTER_ADDR=192.168.0.100 MASTER_PORT=29500 llamafactory-cli train run2node.yaml
